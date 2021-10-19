76bc271e28788c8fd4ac01d8f0891313adcfa835
angelkim88 <74091087+johits@users.noreply.github.com>
2021-10-19 10:57:01 +0900

    apply, when문, databinding 적용

--- app/src/main/java/com/example/naversearch/category/News.kt
+++ app/src/main/java/com/example/naversearch/category/News.kt
@@ -8,13 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.naversearch.adapter.TextAdapter
-import com.example.naversearch.model.NaverAPI
 import com.example.naversearch.ResultGetSearch
-import com.example.naversearch.model.SearchData
+import com.example.naversearch.adapter.TextAdapter
 import com.example.naversearch.databinding.FrgNewsBinding
-import com.example.naversearch.ui.MainActivity
+import com.example.naversearch.model.NaverAPI
+import com.example.naversearch.model.SearchData
 import org.json.JSONArray
 import org.json.JSONObject
 import org.json.JSONTokener
@@ -26,16 +24,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class News : Fragment() {
 
-    private var mBinding: FrgNewsBinding? = null
-    private val binding get() = mBinding!!
-
+    lateinit var binding: FrgNewsBinding
     val BASE_URL_NAVER_API = "https://openapi.naver.com/"
-    lateinit var keyword: String
-    var news = ArrayList<String>()
-
     private val textAdapter = TextAdapter()
     private val sd = mutableListOf<SearchData>()
-
     val reqArray = JSONArray()
 
 
@@ -44,111 +36,119 @@ class News : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mBinding = FrgNewsBinding.inflate(inflater, container, false)
+        binding = FrgNewsBinding.inflate(inflater, container, false)
+
+        binding.apply {
+            fragment = this@News
+            rvNews.adapter = textAdapter
+        }
+
+        return binding.root
+    }
+
+    fun result(v: View) {
 
         val sharedPreferences = requireContext().getSharedPreferences("news", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
+        sd.clear()
 
+        when (v) {
+            binding.btnNews -> {
+                val keyword = binding.etNews.text.toString()
+                Log.d("키워드", "키워드 $keyword")
+                val retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL_NAVER_API)
+                    .addConverterFactory(GsonConverterFactory.create()) //Gson 변환기 사용
+                    .build()
+                val api = retrofit.create(NaverAPI::class.java)
 
-        binding.rvNews.adapter = textAdapter
+                //예외처리 (아무것도 입력하지 않고 검색했을 경우)
+                if (!keyword.equals("")) {
+                    val callGetSearchNews = api.getSearchNews("news", keyword)
+                    callGetSearchNews.enqueue(object : Callback<ResultGetSearch> {
+                        override fun onResponse(
+                            call: Call<ResultGetSearch>,
+                            response: Response<ResultGetSearch>
+                        ) {
 
-        //검색
-        binding.btnNews.setOnClickListener {
-            sd.clear()
-            keyword = binding.etNews.text.toString()
-            Log.d("키워드", "키워드 $keyword")
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL_NAVER_API)
-                .addConverterFactory(GsonConverterFactory.create()) //Gson 변환기 사용
-                .build()
-            val api = retrofit.create(NaverAPI::class.java)
+                            for (item in response.body()?.items!!) {
 
-            //예외처리 (아무것도 입력하지 않고 검색했을 경우)
-            if(!keyword.equals("")) {
-                val callGetSearchNews = api.getSearchNews("news", keyword)
-                callGetSearchNews.enqueue(object : Callback<ResultGetSearch> {
-                    override fun onResponse(
-                        call: Call<ResultGetSearch>,
-                        response: Response<ResultGetSearch>
-                    ) {
+                                //검색 결과 데이터(제목, 내용) 추가하기
+                                sd.add(
+                                    SearchData(
+                                        HtmlCompat.fromHtml(
+                                            item.title,
+                                            HtmlCompat.FROM_HTML_MODE_LEGACY
+                                        ).toString(),
+                                        HtmlCompat.fromHtml(
+                                            item.description,
+                                            HtmlCompat.FROM_HTML_MODE_LEGACY
+                                        ).toString()
+                                    )
+                                )
 
-                        for (item in response.body()?.items!!) {
-
-                            //검색 결과 데이터(제목, 내용) 추가하기
-                            sd.add(
-                                SearchData(
+                                //Json 형식으로 저장
+                                val jsonObject = JSONObject()
+                                jsonObject.put(
+                                    "title",
                                     HtmlCompat.fromHtml(
                                         item.title,
                                         HtmlCompat.FROM_HTML_MODE_LEGACY
-                                    ).toString(),
+                                    )
+                                )
+                                jsonObject.put(
+                                    "description",
                                     HtmlCompat.fromHtml(
                                         item.description,
                                         HtmlCompat.FROM_HTML_MODE_LEGACY
-                                    ).toString()
+                                    )
                                 )
-                            )
+                                jsonObject.put(
+                                    "thumbnail",
+                                    HtmlCompat.fromHtml(
+                                        item.thumbnail,
+                                        HtmlCompat.FROM_HTML_MODE_LEGACY
+                                    )
+                                )
+                                jsonObject.put(
+                                    "link",
+                                    HtmlCompat.fromHtml(item.link, HtmlCompat.FROM_HTML_MODE_LEGACY)
+                                )
 
-                            //Json 형식으로 저장
-                            val jsonObject = JSONObject()
-                            jsonObject.put(
-                                "title",
-                                HtmlCompat.fromHtml(item.title, HtmlCompat.FROM_HTML_MODE_LEGACY)
-                            )
-                            jsonObject.put(
-                                "description",
-                                HtmlCompat.fromHtml(
-                                    item.description,
-                                    HtmlCompat.FROM_HTML_MODE_LEGACY
-                                )
-                            )
-                            jsonObject.put(
-                                "thumbnail",
-                                HtmlCompat.fromHtml(
-                                    item.thumbnail,
-                                    HtmlCompat.FROM_HTML_MODE_LEGACY
-                                )
-                            )
-                            jsonObject.put(
-                                "link",
-                                HtmlCompat.fromHtml(item.link, HtmlCompat.FROM_HTML_MODE_LEGACY)
-                            )
-
-                            reqArray.put(jsonObject)
+                                reqArray.put(jsonObject)
+                            }
+                            textAdapter.addItem(sd)
+                            editor.putString("news", reqArray.toString())
+                            editor.apply()
                         }
-                        textAdapter.addItem(sd)
-                        editor.putString("news", reqArray.toString())
-                        editor.apply()
-                    }
 
 
-                    override fun onFailure(call: Call<ResultGetSearch>, t: Throwable) {
-                        Log.d("MainActivity", "실패 : $t")
-                    }
-                })
-            }else{
-                sd.clear()
-                textAdapter.addItem(sd)
+                        override fun onFailure(call: Call<ResultGetSearch>, t: Throwable) {
+                            Log.d("MainActivity", "실패 : $t")
+                        }
+                    })
+                } else {
+                    sd.clear()
+                    textAdapter.addItem(sd)
+                }
+            }
+
+            binding.btnNewsGet -> {
+                sharedPreferences.getString("news", "")
+
+                //shared prefrence 불러와서 json 파싱하기
+                val jsonArray =
+                    JSONTokener(sharedPreferences.getString("news", null)).nextValue() as JSONArray
+                for (i in 0 until jsonArray.length()) {
+                    val title = jsonArray.getJSONObject(i).getString("title")
+                    val description = jsonArray.getJSONObject(i).getString("description")
+                    sd.add(SearchData(title, description, "", ""))
+                    textAdapter.addItem(sd)
+                }
+
             }
         }
 
-        //조회
-        binding.btnNewsGet.setOnClickListener {
-            sd.clear()
-            sharedPreferences.getString("news", "")
-
-            //shared prefrence 불러와서 json 파싱하기
-            val jsonArray =
-                JSONTokener(sharedPreferences.getString("news", null)).nextValue() as JSONArray
-            for (i in 0 until jsonArray.length()) {
-                val title = jsonArray.getJSONObject(i).getString("title")
-                val description = jsonArray.getJSONObject(i).getString("description")
-                sd.add(SearchData(title,description,"",""))
-
-                textAdapter.addItem(sd)
-            }
-
-        }
-        return binding.root
     }
 
 }
