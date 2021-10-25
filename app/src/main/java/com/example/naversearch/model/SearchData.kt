793b79fcd42b34ba2f542cbda8123d61d9e4e1ea
johits <joker9302@gmail.com>
2021-10-26 00:51:49 +0900

    default 값 제거

--- app/src/main/java/com/example/naversearch/model/SearchData.kt
+++ app/src/main/java/com/example/naversearch/model/SearchData.kt
@@ -2,14 +2,14 @@ package com.example.naversearch.model
 
 data class SearchData(
     // 게시글 제목
-    val title: String = "",
+    val title: String,
 
     // 게시글 내용
-    val description: String = "",
+    val description: String,
 
     //이미지
-    val thumbnail:String? = "",
+    val thumbnail: String?,
 
     //하이퍼텍스트 link
-    val link:String = ""
+    val link: String
 )
