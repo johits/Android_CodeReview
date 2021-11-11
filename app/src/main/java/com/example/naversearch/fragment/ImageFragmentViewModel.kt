b1e0db539513063f6a5d71873d5e860945946577
johits <joker9302@gmail.com>
2021-11-11 23:58:35 +0900

    NaverModel에 있었던 imageadapter 제거

--- app/src/main/java/com/example/naversearch/fragment/ImageFragmentViewModel.kt
+++ app/src/main/java/com/example/naversearch/fragment/ImageFragmentViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.naversearch.fragment
 
-class ImageFragmentViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.naversearch.model.NaverModel
+import com.example.naversearch.model.SearchData
+
+class ImageFragmentViewModel: ViewModel() {
+    private val naverModel = NaverModel("image", "image")
+    private val searchDataModel: LiveData<List<SearchData>>
+        get() = naverModel._searchDataModel
+
+    fun resultBlogSearch(keyword: String) {
+        naverModel.search(keyword)
+    }
+
+    fun getAll(): LiveData<List<SearchData>> {
+        return searchDataModel
+    }
 }
